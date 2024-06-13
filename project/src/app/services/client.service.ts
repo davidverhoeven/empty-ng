cde0082a832ba530e8ed58a80f0b2209cac88284
David Verhoeven <dverhoeven@deloitte.com>
2024-06-13 16:24:54 +0200

    Initial commit

---
+++ project/src/app/services/client.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Client } from "../models/client.model";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CLientService {
+
+
+    baseUrl: string = "";
+
+    constructor(private http: HttpClient) { }
+
+    getById(clientId: string) {
+        return this.http.get<Client>(`${this.baseUrl}/${clientId}`);
+    }
+
+
+    getAll(): Observable<Array<Client>> {
+        return this.http.get<Array<Client>>(this.baseUrl);
+    }
+
+    insert(client: Client):Observable<string>{
+        return this.http.post<string>(this.baseUrl, client);
+    }
+
+}
